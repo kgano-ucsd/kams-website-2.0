e1e39ffef84f391186e9243821716bf0b786dd4e
Kameron Gano <103277185+kgano-ucsd@users.noreply.github.com>
2023-03-10 15:31:57 -0800

    More setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  daisyui: {
+    themes: [
+      {
+        kameronpalette: {
+          primary: "#404E7C",
+          secondary: "#E9E3E6",
+          accent: "#8789C0",
+          neutral: "#FAF9F6",
+          "base-100": "#FFFFFF",
+          info: "#D5D5D5",
+          success: "#48DB72",
+          warning: "#FBBD23",
+          error: "#F87272",
+        },
+      },
+    ],
+  },
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ['"Rubik"', "sans-serif"],
+      body: ["Cabin", "sans-serif"],
+      display: ["Helvetica", "sans-serif"],
+    },
   },
-  plugins: [],
-}
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+};
