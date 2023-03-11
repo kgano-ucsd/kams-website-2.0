a2b426afc1525ab93afb77b1f7440d725a9b04c8
Kameron Gano <103277185+kgano-ucsd@users.noreply.github.com>
2023-03-11 12:58:16 -0800

    Minor changes and fixed navbar css

--- kams-website-v2/src/atoms/Banner.jsx
+++ kams-website-v2/src/atoms/Banner.jsx
@@ -3,18 +3,18 @@ export default function({ children }) {
 
     return (
         <>
-            <div className="w-screen h-1/8 flex flex-row p-2">
-                <div className="w-2/3">
-                    <h1 className="text-xl font-semibold font-display p-4 ml-1 text-white">
+            <div className="w-screen h-1/8 flex flex-row p-4">
+            <div className="flex-grow border-box">
+                    <h1 className="text-xl font-semibold font-display ml-1 p-4 text-left text-white">
                         kameron gano
                     </h1>
                 </div>
-                <div className="w-1/6 grid place-items-center">
+                <div className="w-1/6 text-center ">
                     <h2 className="text-lg font-medium font-display p-4 text-white">
                         about
                     </h2>
                 </div>
-                <div className="w-1/6 grid place-items-center">
+                <div className="w-1/6 text-center ">
                     <h2 className="text-lg font-medium font-display p-4 text-white">
                         projects
                     </h2>
--- kams-website-v2/src/components/Hello.jsx
+++ kams-website-v2/src/components/Hello.jsx
@@ -6,11 +6,13 @@ export default function Hello() {
     return (
         <div className="grid place-items-center h-full">
             <h1 className="text-3xl font-semibold font-display text-white" >
-                {"hi, my name is "}
+                {"hi, my name is kameron gano and i study "}
                 <span className="magic">
-                <span className="magic-text"> kameron gano </span> 
+                    <span className="magic-text">
+                        computer engineering
+                    </span>
                 </span>
-                {" and i study computer engineering at ucsd."}
+                {" at uc san diego."}
             </h1>
         </div>
     )
