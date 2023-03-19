baa10c98cf11a79e6f592a7663aa5dcd408cd053
Kameron Gano <103277185+kgano-ucsd@users.noreply.github.com>
2023-03-19 00:39:38 -0700

    Got interactive buttons to work

--- kams-website-v2/src/atoms/Banner.jsx
+++ kams-website-v2/src/atoms/Banner.jsx
@@ -1,3 +1,5 @@
+import "../index.css"
+
 
 export default function Banner() {
 
@@ -7,20 +9,22 @@ export default function Banner() {
             <div className="flex-grow border-box">
                     <h1 
                         style={{fontFamily: "Lobster"}}
-                        className="text-2xl font-semibold font-display ml-1 p-4 text-left text-white">
+                        className="text-2xl font-semibold font-display p-3 text-left text-white">
                         kameron gano
                     </h1>
                 </div>
                 <div className="w-1/6 text-center ">
-                    <a href="#about"> 
-                        <h2 className="text-lg font-medium font-display p-4 text-white">
+                    <h2 className="transform text-lg font-medium font-display p-4 text-white hover:transition duration-500 hover:font-semibold hover:scale-100">
+                        <a href="#about"> 
                             about
-                        </h2>
-                    </a>
+                        </a>
+                    </h2>
                 </div>
                 <div className="w-1/6 text-center ">
-                    <h2 className="text-lg font-medium font-display p-4 text-white">
-                        projects
+                    <h2 className="transform text-lg font-medium font-display p-4 text-white hover:transition duration-500 hover:font-semibold hover:scale-100">
+                        <a href="">
+                            projects
+                        </a>
                     </h2>
                 </div>
                 
