18ac8da89850a5d0616d524471a8f732527bd64e
Kameron Gano <103277185+kgano-ucsd@users.noreply.github.com>
2023-07-11 10:33:33 +0100

    i tried lol

--- kams-website-v2/src/pages/Projects.jsx
+++ kams-website-v2/src/pages/Projects.jsx
@@ -14,19 +14,42 @@ export default function Projects() {
           >
             <div className="w-1/3 p-2">
                 <div className="test">
-                    <span className=" font-bold text-white text-4xl p-3">
+                    <h1 className=" font-bold text-white text-4xl p-3">
                         stanford aimi
-                    </span>
+                    </h1>
+                    {"\n"}
+                    <div className="font-medium text-white text-2xl p-3">
+                        {"i was a student lead for the 2023 summer research internship \
+                        at the stanford aimi center. \
+                        here, i led to group of high school students to train ml models \
+                        that classified and predicted the placement of endotracheal tubes \
+                        with respect to the carina using chest ct scans."}
+                    </div>
+                    {"\n"}
+                    <h1 className="font-bold text-white text-3xl p-3">
+                        {"awards"}
+                        <div className="font-medium flex flex-row">
+                        
+                        </div>
+                    </h1>
+                    {"\n"}
+                    <h1 className="font-medium text-white text-3xl p-3">
+                        github repo
+                    </h1>
                 </div>
             </div>
             <div className="w-1/3 p-2">
                 <div className="test">
-
+                    <h1 className=" font-bold text-white text-4xl p-3">
+                        oloren chemengine
+                    </h1>
                 </div>
             </div>
             <div className="w-1/3 p-2">
                 <div className="test">
-
+                    <h1 className=" font-bold text-white text-4xl p-3">
+                        treehacks 2023
+                    </h1>
                 </div>
             </div>
           </section>
