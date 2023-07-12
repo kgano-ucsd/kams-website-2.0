7b9e04ed38e68343847fac19082b8356ba0e3ea9
Kameron Gano <103277185+kgano-ucsd@users.noreply.github.com>
2023-07-12 10:18:36 +0100

    shtuff

--- kams-website-v2/src/pages/Projects.jsx
+++ kams-website-v2/src/pages/Projects.jsx
@@ -4,13 +4,14 @@ import "../index.css";
 export default function Projects() {
 
     return(
+        
         <section 
             id="projects"
             style=
             {{backgroundImage: `url(${about})`,
               backgroundRepeat: false,
               backgroundSize: "cover"}}
-              className="flex flex-row p-6 w-full h-full relative"
+              className="flex flex-row p-10 gap-6 w-full h-full relative"
           >
             <div className="w-1/3 p-2">
                 <div className="test">
@@ -19,23 +20,11 @@ export default function Projects() {
                     </h1>
                     {"\n"}
                     <div className="font-medium text-white text-2xl p-3">
-                        {"i was a student lead for the 2023 summer research internship \
-                        at the stanford aimi center. \
-                        here, i led to group of high school students to train ml models \
-                        that classified and predicted the placement of endotracheal tubes \
-                        with respect to the carina using chest ct scans."}
+                        <li>
+                            Pee Pee Poo Poo 
+                        </li>
                     </div>
-                    {"\n"}
-                    <h1 className="font-bold text-white text-3xl p-3">
-                        {"awards"}
-                        <div className="font-medium flex flex-row">
-                        
-                        </div>
-                    </h1>
-                    {"\n"}
-                    <h1 className="font-medium text-white text-3xl p-3">
-                        github repo
-                    </h1>
+
                 </div>
             </div>
             <div className="w-1/3 p-2">
@@ -45,7 +34,7 @@ export default function Projects() {
                     </h1>
                 </div>
             </div>
-            <div className="w-1/3 p-2">
+            <div className="w-1/3 p-2 py-4">
                 <div className="test">
                     <h1 className=" font-bold text-white text-4xl p-3">
                         treehacks 2023
